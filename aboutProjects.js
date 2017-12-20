1807a9ccf2a2421a57ae20d8d919d35842330270
thehelini <helena20817@gmail.com>
2017-12-20 00:20:53 -0800

    Delete aboutProjects.js

--- aboutProjects.js
+++
@@ -1,9 +0,0 @@
-$(".text").mouseover(function addBorder() {
-    $(this).addClass("borderClass");
-});
-
-$(".text").mouseout(function removeBorder() {
-    $(this).removeClass("borderClass");
-});
-
-$(".text").draggable();
