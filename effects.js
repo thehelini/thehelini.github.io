3080890c067819734fd99ea3a9b585a474bb4e7f
Helena Li <helena20817@berkeley.edu>
2017-12-20 17:21:24 -0800

    new thought and fixed some glitches

--- effects.js
+++ effects.js
@@ -8,8 +8,17 @@ $(".text").mouseout(function removeBorder() {
 
 $(".text").draggable();
 
-//randomizing thought boxes
+//CSS FOR THOUGHTS PAGE
+$(".box").mouseover(function addBorder() {
+    $(this).addClass("borderClass");
+});
+
+$(".box").mouseout(function removeBorder() {
+    $(this).removeClass("borderClass");
+});
+//puts thought boxes in array
 var divArray = [];
+var highestIndex = 0;
 $(".box").each(function () {
     divArray.push($(this));
 });
@@ -24,11 +33,15 @@ function generateRandom() {
     var num = Math.floor(Math.random()*60);
     return num;
 };
+//randomizes how the boxes appear
 function randomnize() {
     for (var i = 0; i < divArray.length; i++){
       var left = generateRandom();
       var top = generateRandom();
       var index = Math.floor(Math.random()*divArray.length+1);
+      if (index >= highestIndex) {
+        highestIndex = index;
+      };
       divArray[i].css({
         "position": "absolute", "top": top+"vh", "left": left+"vw", "zIndex": index
     });
